5d56f53245f2f5db11ea73470a25c0b2de752c49
Jason <113925541+JasonTartanga@users.noreply.github.com>
2022-10-21 22:20:54 +0200

    Update DNI2.java

--- DNI2.java
+++ DNI2.java
@@ -2,50 +2,117 @@ package DNI;
 
 import java.util.Scanner;
 
-public class DNI2 {
+public class DNI1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		char opc = 0;
-		String letraDNI = "TRWAGMYFPDXBNJZSQVLCKE";
-		int numLetras = 0;
-		int resto = 0;
-		boolean tieneLetra = false;
-		
+
+		String Letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+		String DNIconLetra;
+		int DNIsinLetra;
+		int resto;
+		int Digitos = 0;
+		Boolean LetraFinal = false;
+		Boolean Terminado = false;
+
 		do {
-			System.out.println();
-		
-		System.out.print("Introduce tu DNI (con letra): ");
-		String DNI = sc.next().toUpperCase();
+			System.out.print("Introduce tu DNI (sin letra): ");
+			DNIsinLetra = sc.nextInt();
+			resto = DNIsinLetra % 23;
 
-		
-
-		if (DNI.length() != 9) {
-			System.out.println("El formato del DNI es erroneo");
-		} else {
-			for (int i = 0; i < DNI.length() - 1; i++) {
-				if (Character.isDigit(DNI.charAt(i))) {
-					numLetras++;
-				} else {
-					System.out.println("El formato del DNI es erroneo");
-				}
-			}
-			if (!Character.isDigit(DNI.charAt(8))) {
-				tieneLetra = true;
+			if (Integer.toString(DNIsinLetra).length() == 8) {
+				System.out.println("Tu DNI con letra es: " + DNIsinLetra + Letras.charAt(resto));
+				Terminado = true;
 			} else {
-				System.out.println("El formato del DNI es erroneo");
+				System.out.println("El formato del DNI es erroneo!");
 			}
-			if (numLetras == 8 && tieneLetra) {
-				resto = Integer.parseInt(DNI.substring(0, 8)) % 23;
-				if (DNI.charAt(8) == letraDNI.charAt(resto)) {
-					System.out.println("La letra introducida es correcta");
+
+		} while (!Terminado);
+
+		System.out.println();
+		do {
+			Terminado = false;
+
+			System.out.print("Introduce tu DNI (con letra): ");
+			DNIconLetra = sc.next().toUpperCase();
+
+			if (DNIconLetra.length() != 9) {
+				System.out.println("El formato del DNI es erroneo");
+			} else {
+				for (int i = 0; i < DNIconLetra.length() - 1; i++) {
+					if (Character.isDigit(DNIconLetra.charAt(i))) {
+						Digitos++;
+					}
+				}
+				if (!Character.isDigit(DNIconLetra.charAt(8))) {
+					LetraFinal = true;
+				}
+				if (Digitos == 8 && LetraFinal) {
+					resto = Integer.parseInt(DNIconLetra.substring(0, 8)) % 23;
+
+					if (DNIconLetra.charAt(8) == Letras.charAt(resto)) {
+						System.out.println("El DNI que has introducido es correcto");
+					} else {
+						System.out.println("El DNI es incorrecto tu letra es: " + Letras.charAt(resto));
+					}
+
+					Terminado = true;
 				} else {
-					System.out.println("Incorrecto. La letra de tu DNI es: " + letraDNI.charAt(resto));
+					System.out.println("El formato del DNI es erroneo!");
 				}
 			}
-		}
+		} while (!Terminado);
 
+		System.out.println();
+		do {
+			Terminado = false;
+			Digitos = 0;
+			
+			System.out.print("Introduce tu DNI o NIE: ");
+			DNIconLetra = sc.next().toUpperCase();
+
+			if (!Character.isDigit(DNIconLetra.charAt(0))) {
+				
+				switch (DNIconLetra.charAt(0)) {
+				case 'X':
+					DNIconLetra = 0 + DNIconLetra.substring(1, 9);
+					break;
+				case 'Y':
+					DNIconLetra = 1 + DNIconLetra.substring(1, 9);
+					break;
+				case 'Z':
+					DNIconLetra = 2 + DNIconLetra.substring(1, 9);
+					break;
+				}
+
+			}
+			if (DNIconLetra.length() != 9) {
+				System.out.println("El formato del DNI es erroneo");
+			} else {
+				for (int i = 0; i < DNIconLetra.length() - 1; i++) {
+					if (Character.isDigit(DNIconLetra.charAt(i))) {
+						Digitos++;
+					}
+				}
+				if (!Character.isDigit(DNIconLetra.charAt(8))) {
+					LetraFinal = true;
+				}
+				if (Digitos == 8 && LetraFinal) {
+					resto = Integer.parseInt(DNIconLetra.substring(0, 8)) % 23;
+
+					if (DNIconLetra.charAt(8) == Letras.charAt(resto)) {
+						System.out.println("El DNI que has introducido es correcto");
+					} else {
+						System.out.println("El DNI es incorrecto tu letra es: " + Letras.charAt(resto));
+					}
+
+					Terminado = true;
+				} else {
+					System.out.println("El formato del DNI es erroneo!");
+				}
+			}
+
+		} while (!Terminado);
 		sc.close();
 	}
-
 }

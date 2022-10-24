25ce572553c1d419e8d45cc4e7363809613d15c4
Jason <113925541+JasonTartanga@users.noreply.github.com>
2022-10-24 10:34:20 +0200

    Update Ejercicio8.java

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -4,23 +4,56 @@ public class Ejercicio8 {
 
 	public static void main(String[] args) {
 
-		String primerPlato[] = { "Ensalada  ", "Estofado  ", "Macarrones", "Risotto   ", "Garbanzos ", "Sopa      ",
+		String primerPlato[] = { "Ensalada  ", "Spagetti  ", "Macarrones", "Risotto   ", "Garbanzos ", "Sopa      ",
 				"Lentejas  ", "Canelones " };
 		String segundoPlato[] = { "Estofado", "Hamburguesa", "Costillas", "Salmon", "Lomo", "Pollo", "Lasaña",
 				"Albondigas", "Pizza", "Kebab" };
 		String Dia[] = { "Lunes    ", "Martes   ", "Miercoles", "Jueves  ", "Viernes  ", "Sabado   ", "Domingo  " };
 
-
+		int[] repetido = new int[8];
+		int[] repetido2 = new int[10];
+		int primero = 0;
+		int segundo = 0;
+		boolean iguales1;
+		boolean iguales2;
+		
 		System.out.println("\t\t\tMENU SEMANAL");
 		System.out.println();
 		System.out.println("         \tPRIMERO \tSEGUNDO");
 		System.out.println();
 
 		for (int i = 0; i < Dia.length; i++) {
-			int primero = (int) (Math.random() * 8);
-			int segundo = (int) (Math.random() * 10);
+			primero = (int) (Math.random() * 8);
+			iguales1 = false;
+			for (int j = 0; j < repetido.length && !iguales1; j++) {
+				if (repetido[j] == primero) {
+					iguales1 = true;
+				}
+			}
+			if (!iguales1) {
+				repetido[i] = primero;
+			} else {
+				i--;
+			}
+		}
 
-			System.out.println(Dia[i] + "\t" + primerPlato[primero] + "\t" + segundoPlato[segundo]);
+		for (int i = 0; i < Dia.length; i++) {
+			segundo = (int) (Math.random() * 10);
+			iguales2 = false;
+			for (int j = 0; j < repetido2.length && !iguales2; j++) {
+				if (repetido2[j] == primero) {
+					iguales2 = true;
+				}
+			}
+			if (!iguales2) {
+				repetido[i] = segundo;
+
+			} else {
+				i--;
+			}
+		}
+		for (int i = 0; i < Dia.length; i++) {
+			System.out.println(Dia[i] + "\t" + primerPlato[i] + "\t" + segundoPlato[i]);
 		}
 	}
 }
